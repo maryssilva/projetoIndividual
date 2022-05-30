7a60e32d519e3415e167eb3aadd449cef28396d7
Maryanna Silva <maryanna.silva@sptech.school>
2022-05-30 10:38:08 -0300

    Tentando fazer a API funcionarrrrr

--- Site/site/src/models/avisoModel.js
+++ Site/site/src/models/avisoModel.js
@@ -64,10 +64,7 @@ function listarPorUsuario(idUsuario) {
 
 function publicar(selectCidades, comentario, idUsuario) {
     console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function publicar(): ", selectCidades, comentario, idUsuario);
-    var instrucao = `
-        INSERT INTO cidades (cidade) VALUES ('${selectCidades}');
-        INSERT INTO avaliacao (fkCidade, comentario, fkUsuario) VALUES ('1', '${comentario}', ${idUsuario});
-    `;
+    var instrucao = `INSERT INTO cidades (cidade) VALUES ('${selectCidades}'); INSERT INTO avaliacao (fkCidade, comentario, fkUsuario) VALUES ('1', '${comentario}', ${idUsuario});`;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
